5464a86829ee7a0227b410e6c989f1f8fd858486
Filip Klembara <filip@klembara.pro>
2017-10-27 17:31:39 +0200

    Revert "swift 4.1"
    This reverts commit f63857ef58ec7ffbbe014436341ffffe964924a9.

--- Sources/Cache/Storage/DiskStorage.swift
+++ Sources/Cache/Storage/DiskStorage.swift
@@ -191,9 +191,15 @@ final class DiskStorage: StorageAware {
             guard fileManager.fileExists(atPath: url.absoluteString, isDirectory: &isDir) else {
                 continue
             }
-            guard isDir.boolValue == false else {
-                continue
-            }
+            #if os(Linux)
+                guard isDir == false else {
+                    continue
+                }
+            #else
+                guard isDir.boolValue == false else {
+                    continue
+                }
+            #endif
 
             #if os(Linux)
                 var stat_struct = stat()
