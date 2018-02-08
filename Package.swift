68b6c4039446f58c88e027f746fbfdf57a85f842
Filip Klembara <filip@klembara.pro>
2018-02-08 16:21:04 +0100

    rename

--- Package.swift
+++ Package.swift
@@ -6,18 +6,18 @@ let package = Package(
     name: "SquirrelCache",
     products: [
         .library(
-            name: "Cache",
-            targets: ["Cache"]),
+            name: "SquirrelCache",
+            targets: ["SquirrelCache"]),
     ],
     dependencies: [
         .package(url: "https://github.com/onmyway133/SwiftHash.git", from: "2.0.0")
     ],
     targets: [
         .target(
-            name: "Cache",
+            name: "SquirrelCache",
             dependencies: ["SwiftHash"]),
         .testTarget(
             name: "MacTests",
-            dependencies: ["Cache"]),
+            dependencies: ["SquirrelCache"]),
     ]
 )
